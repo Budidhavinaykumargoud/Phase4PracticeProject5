e1ae818ed3efe31dd4ac51792b406929aac275c4
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2024-01-18 22:33:55 +0530

    Finalcommit

---
+++ FeedbackForm.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PracticeProject5
+{
+    public partial class FeedbackForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LblMsg.Visible = false;
+        }
+
+        protected void Btnsubmit_Click(object sender, EventArgs e)
+        {
+            LblMsg.Visible = true;
+            SqlConnection con = new SqlConnection("Server=tcp:project18.database.windows.net,1433;Initial Catalog=vinn654db;Persist Security Info=False;User ID=vinaygoud123;Password=Viratkohli49*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("insert into FeedbackForm(EmployeeId,Name,Email,Message) values(@EmployeeId,@Name,@mail,@Message) ", con);
+                cmd.Parameters.AddWithValue("@EmployeeId", int.Parse(Txtid.Text));
+                cmd.Parameters.AddWithValue("@Name", Txtname.Text);
+                cmd.Parameters.AddWithValue("@mail", Txtmail.Text);
+                cmd.Parameters.AddWithValue("@Message", TxtMessage.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                LblMsg.Text = "Feedback Submitted!!!";
+
+
+            }
+            catch (Exception ex)
+            {
+                LblMsg.Text += "Error!!!" + ex.Message;
+            }
+            finally { con.Close(); }
+        }
+    }
+}
